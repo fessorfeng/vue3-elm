b46311c64b945792b1f79f871afff33286d0d6cc
fengjingtao <352475718@qq.com>
2022-05-04 10:23:10 +0800

    feat:打包优化加入测试plugin,打包体积plugin,分布策略

---
+++ build/webpack.dll.conf.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    vue: ["vue", "vue-router", "vuex"],
+    scroll: ["better-scroll", "iscroll"],
+    showdown: ["showdown"],
+  },
+  output: {
+    path: path.resolve(__dirname, "../dll"),
+    filename: "[name]_[fullhash:6].dll.js",
+    library: "[name]_[fullhash:6]",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DllPlugin({
+      context: process.cwd(),
+      path: path.resolve(__dirname, "../dll", "[name].manifest.json"),
+      name: "[name]_[fullhash:6]",
+    }),
+  ],
+};
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
 const path = require("path");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const webpack = require("webpack");
+
+// 使用启用了Measure模式（打包测速）
+const isMeasure = process.env.MEASURE === "true";
+const smp = new SpeedMeasurePlugin({
+  disable: !isMeasure,
+});
 
 module.exports = {
-  configureWebpack: {
+  // publicPath: "./",
+  configureWebpack: smp.wrap({
     resolve: {
       alias: {
         src: path.resolve(__dirname, "./src"),
@@ -9,5 +19,22 @@ module.exports = {
         components: path.resolve(__dirname, "./src/components"),
       },
     },
-  },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: isMeasure ? "server" : "disabled",
+      }),
+      new webpack.DllReferencePlugin({
+        context: process.cwd(),
+        manifest: path.resolve(__dirname, './dll/vue.manifest.json'),
+      }),
+      new webpack.DllReferencePlugin({
+        context: process.cwd(),
+        manifest: path.resolve(__dirname, './dll/scroll.manifest.json'),
+      }),
+      new webpack.DllReferencePlugin({
+        context: process.cwd(),
+        manifest: path.resolve(__dirname, './dll/showdown.manifest.json'),
+      })
+    ],
+  }),
 };
