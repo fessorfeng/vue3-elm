d701f3d9f8272f55bee731375fb1edfae266c49d
fengjingtao <fengjt19@midea.com>
2022-05-05 15:38:50 +0800

    打包报错三个warnings 暂时是调大max对应的performance隐藏掉
    说明：
    dev 可以该配置devServer: {
        static: {
          directory: path.join(__dirname, 'public'),
        },
      },
    打包报错三个warnings 暂时是调大max对应的performance隐藏掉；因为不熟悉项目不知道再抽哪个出来cachegroup;
    拆包分dev与prod模式

--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -1,16 +1,21 @@
 const path = require("path");
 const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
+const isDev = process.env.NODE_ENV === "development" || process.env.DEV === 'true';
+const dllPath = isDev ? '../dll/dev' : '../dll/prod';
+
+// console.log(process.env.NODE_ENV, isDev)
 module.exports = {
-  mode: "production",
+  mode: isDev ? "development" : "production",
   entry: {
     vue: ["vue", "vue-router", "vuex"],
-    scroll: ["better-scroll", "iscroll"],
+    scroll: ["better-scroll", "iscroll", "fastclick"],
     showdown: ["showdown"],
   },
   output: {
-    path: path.resolve(__dirname, "../dll"),
+    path: path.resolve(__dirname, dllPath),
     filename: "[name].dll.js",
     library: "[name]_[fullhash:6]",
   },
@@ -18,8 +23,22 @@ module.exports = {
     new CleanWebpackPlugin(),
     new webpack.DllPlugin({
       context: process.cwd(),
-      path: path.resolve(__dirname, "../dll", "[name].manifest.json"),
+      path: path.resolve(__dirname, dllPath, "[name].manifest.json"),
       name: "[name]_[fullhash:6]",
     }),
   ],
+  optimization: {
+    minimize: true, //代码压缩
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
 };
--- vue.config.js
+++ vue.config.js
@@ -4,22 +4,30 @@ const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const webpack = require("webpack");
 const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
 
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 // 使用启用了Measure模式（打包测速）
 const isMeasure = process.env.MEASURE === "true";
 const isAnalyze = process.env.ANALYZE === "true";
+const isDev = process.env.NODE_ENV === "development";
 const smp = new SpeedMeasurePlugin({
   disable: !isMeasure,
 });
 
-const glob = require('glob')
-const PurgeCSSPlugin = require('purgecss-webpack-plugin')
+const glob = require("glob");
+const PurgeCSSPlugin = require("purgecss-webpack-plugin");
 const PATHS = {
-  src: path.join(__dirname, 'src')
-}
+  src: path.join(__dirname, "src"),
+};
+const dllPath = isDev ? './dll/dev' : './dll/prod';
+
+// console.log(process.env.NODE_ENV, isDev);
 
 module.exports = {
   // publicPath: "./",
   configureWebpack: smp.wrap({
+    devtool: isDev ? 'eval-source-map' :  false,
     // 利用缓存提升二次构建速度
     cache: {
       type: "filesystem",
@@ -29,36 +37,65 @@ module.exports = {
         "node_modules/.webpack_temp_cache"
       ),
     },
-    module: {
-      rules: [{
-        test: /\.(gif|png|jpe?g|svg|webp)$/i,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75
-              }
-            }
+    performance: {
+      hints: isDev ? false : "warning", // 枚举 false关闭
+      maxEntrypointSize: 500 * 1024, // 最大入口文件大小
+      maxAssetSize: 500 * 1024, // 最大资源文件大小
+    },
+    optimization: {
+      minimize: true, //代码压缩
+      minimizer: [
+        // 这将仅在生产环境开启 CSS 优化。
+        new CssMinimizerPlugin(),
+        new TerserPlugin({
+          test: /\.js(\?.*)?$/i,
+          terserOptions: {
+            format: {
+              comments: false,
+            },
           },
-        ],
-      }]
+          extractComments: false,
+        }),
+      ],
+      splitChunks: {
+        chunks: "all",
+        minSize: 1 * 1024,
+        minChunks: 1,
+        name: "common",
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(gif|png|jpe?g|svg|webp)$/i,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "image-webpack-loader",
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                // optipng.enabled: false will disable optipng
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.9],
+                  speed: 4,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                // the webp option will enable WEBP
+                webp: {
+                  quality: 75,
+                },
+              },
+            },
+          ],
+        },
+      ],
     },
     resolve: {
       alias: {
@@ -73,23 +110,23 @@ module.exports = {
       }),
       new webpack.DllReferencePlugin({
         context: process.cwd(),
-        manifest: path.resolve(__dirname, "./dll/vue.manifest.json"),
+        manifest: path.resolve(__dirname, dllPath, "vue.manifest.json"),
       }),
       new webpack.DllReferencePlugin({
         context: process.cwd(),
-        manifest: path.resolve(__dirname, "./dll/scroll.manifest.json"),
+        manifest: path.resolve(__dirname, dllPath, "scroll.manifest.json"),
       }),
       new webpack.DllReferencePlugin({
         context: process.cwd(),
-        manifest: path.resolve(__dirname, "./dll/showdown.manifest.json"),
+        manifest: path.resolve(__dirname, dllPath, "showdown.manifest.json"),
       }),
       new AddAssetHtmlPlugin([
-        { filepath: path.resolve(__dirname, "./dll/vue.dll.js") },
-        { filepath: path.resolve(__dirname, "./dll/scroll.dll.js") },
-        { filepath: path.resolve(__dirname, "./dll/showdown.dll.js") },
+        { filepath: path.resolve(__dirname, dllPath, "vue.dll.js") },
+        { filepath: path.resolve(__dirname, dllPath, "scroll.dll.js") },
+        { filepath: path.resolve(__dirname, dllPath, "showdown.dll.js") },
       ]),
       new PurgeCSSPlugin({
-        paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+        paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
       }),
     ],
   }),
